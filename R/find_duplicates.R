d1338cd91c92ba5988cf01049eaf54efa01104d3
Daniel Dang <dnld1ng@yahoo.com>
2026-01-15 15:24:56 -0500

    Fleshed out functions for finding duplicate rows

--- R/find_duplicates.R
+++ R/find_duplicates.R
@@ -1,14 +1,30 @@
 
 # Find duplicate rows for a csv file
-find_duplicates <- function() {
+find_duplicates <- function(data) {
   if (is.character(data)){
     data <- read.csv(data)
   }
   if (!is.data.frame(data)){
     stop("data must be a csv file or a data frame")
   }
+  # Add column for tracking original positions of the rows
+  data$original_row_id <- 1:nrow(data)
+
+  is_duplicate <- duplicated(data[,-ncol(data)], fromLast = TRUE) | duplicated(data[,-ncol(data)])
+  duplicates <- data[is_duplicate,]
   
-  View(data)
+  total_duplicates <- nrow(duplicates)
+  if (nrow(duplicates) > 0){
+    print(paste("Found", total_duplicates, "duplicate rows"))
+    print(duplicates)
+  }
+  else{
+    print("No duplicates found")
+  }
+
   
-  return(0)
+  return(total_duplicates)
 }
+
+find_duplicates(iris)
+find_duplicates(airquality)
--- tests/test_find_duplicates.R
+++ tests/test_find_duplicates.R
@@ -2,9 +2,17 @@ library(testthat)
 source("../R/find_duplicates.R")
 
 test_that("able to read dataframe", {
+  # Found 0 duplicates
   expect_equal(find_duplicates(airquality), 0)
+  
+  # Found non-zero duplicates
+  expect_equal(find_duplicates(iris), 2)
 })
 
 test_that("able to read csv file", {
-  expect_equal(find_duplicates("dirty_data.csv"), 0)
+  expect_equal(find_duplicates("dirty_data.csv"), 10)
+})
+
+test_that("able to handle errors", {
+  expect_error(find_duplicates(1), "data must be a csv file or a data frame")
 })
