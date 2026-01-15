77e93bb2c08e32d6d1938e28ab39d70c00d11dc7
Daniel Dang <dnld1ng@yahoo.com>
2026-01-14 20:29:55 -0500

    Added test cases

--- tests/test_find_nulls.R
+++ tests/test_find_nulls.R
@@ -1,10 +1,21 @@
 library(testthat)
 source("../R/find_nulls.R")
 
-test_that("able to read dataframe", {
-  expect_equal(find_nulls(airquality), 0)
+test_that("able to read dataframe and return correct count of na values", {
+  result <- find_nulls(airquality)
+  expect_equal(sum(result$count), 44)
+  
+  # Able to handle data with no missing values
+  result <- find_nulls(cars)
+  expect_equal(sum(result$count), 0)
+  expect_equal(sum(result$percent), 0)
 })
 
 test_that("able to read csv file", {
-  expect_equal(find_nulls("dirty_data.csv"), 0)
+  result <- find_nulls("dirty_data.csv")
+  expect_equal(sum(result$count), 46)
+})
+
+test_that("able to handle errors", {
+  expect_error(find_nulls(1), "data must be a csv file or a data frame")
 })
