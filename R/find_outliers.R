e47e42dce4a41d72cafdd1e2f072c83ddf1bd822
Daniel Dang <dnld1ng@yahoo.com>
2026-01-16 19:28:24 -0500

    Fleshed out function for finding outliers

--- R/find_outliers.R
+++ R/find_outliers.R
@@ -1,14 +1,42 @@
 
 # Find outliers for each numerical column for a csv file
-find_outliers <- function() {
+find_outliers <- function(data) {
   if (is.character(data)){
     data <- read.csv(data)
   }
   if (!is.data.frame(data)){
     stop("data must be a csv file or a data frame")
   }
-  
-  View(data)
-  
-  return(0)
+  # Keep track of what columns are numeric
+  number_cols <- sapply(data, is.numeric)
+
+  outlier_report <- list()
+  # Going through each numerical column
+  for(col in names(data[number_cols])){
+    vals <- data[[col]]
+    
+    q1 <- quantile(vals, 0.25, na.rm = TRUE)
+    q3 <- quantile(vals, 0.75, na.rm = TRUE)
+    iqr <- q3 - q1
+    
+    lower_limit <- q1 - (1.5 * iqr)
+    upper_limit <- q3 + (1.5 * iqr)
+    
+    outliers <- which(vals < lower_limit | vals > upper_limit)
+    
+    if (length(outliers) > 0){
+      report <- data.frame(
+        rows = outliers,
+        vals = vals[outliers],
+        lower_limit = lower_limit,
+        upper_limit = upper_limit
+      )
+      
+      outlier_report[[col]] <- report
+    }
+    
+  }
+  return(outlier_report)
 }
+
+find_outliers(airquality)
